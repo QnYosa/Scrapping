8d5a107335eeb509f974972302577418d7e6b013
QnYosa <55686931+QnYosa@users.noreply.github.com>
2019-10-11 09:00:42 +0200

    Update 00_dark_trader_rspec.rb

--- spec/00_dark_trader_rspec.rb
+++ spec/00_dark_trader_rspec.rb
@@ -1,5 +1,19 @@
-require_relative '..lib/dark_trader'
+require_relative '../lib/dark_trader'
 
-describe "#name" do 
-    it "lists all the cryptocurrencies existing" do 
-        
+describe "the name_ method" do
+  it "should return an array of currencies names not nil" do
+    expect(name_(Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")))).not_to be_nil
+  end
+end
+
+describe "the price_list method" do
+  it "should return an array of currencies prices not nil" do
+    expect(price_list(Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")))).not_to be_nil
+  end
+end
+
+describe "the c_value method" do
+  it "should return an array of hashes with currencies names and value not nil" do
+    expect(c_value(Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")))).not_to be_nil
+  end
+end
